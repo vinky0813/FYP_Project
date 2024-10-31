d6145c3e973ae2aa556f39d483cfb7b20a8456fe
kelvinkyy <kelvinkyy03@gmail.com>
2024-10-31 22:06:57 +0800

    quick api update

--- api/index.js
+++ api/index.js
@@ -50,8 +50,7 @@ async function authenticateToken(req, res, next) {
     if (error || !user) {
       return res.status(403).json({ error: 'Invalid token' });
     }
-    
-    supabase.auth.setAuth(token);
+
     req.user = user;
 
     next();
